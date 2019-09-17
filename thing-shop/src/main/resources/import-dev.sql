11cab43e739d273d8cb27c602ffb07d29336405a
Maximillian Arruda <dearrudam@gmail.com>
2019-09-17 02:42:17 -0300

    Added SQL scripts, but a test needs to be performed

--- thing-shop/src/main/resources/import-dev.sql
+++ thing-shop/src/main/resources/import-dev.sql
@@ -1,9 +1,9 @@
-INSERT INTO public.item (id,amount,currency,"name") VALUES (1,1000.00,'BRL','mother fucker pencil');
-INSERT INTO public.item (id,amount,currency,"name") VALUES (2,1000.00,'BRL','mother fucker pen');
-INSERT INTO public.item (id,amount,currency,"name") VALUES (3,1000.00,'BRL','mother fucker brush');
-INSERT INTO public.item (id,amount,currency,"name") VALUES (4,1000.00,'BRL','mother fucker cleaner');
-INSERT INTO public.item (id,amount,currency,"name") VALUES (5,1000.00,'BRL','mother fucker notes');
-INSERT INTO public.item (id,amount,currency,"name") VALUES (6,1000.00,'BRL','mother fucker bag');
+INSERT INTO public.item (id,amount,currency,"name") VALUES (1,1000.00,'BRL','the mother fuc!@#!$ pencil');
+INSERT INTO public.item (id,amount,currency,"name") VALUES (2,1000.00,'BRL','the mother fuc!@#!$ pen');
+INSERT INTO public.item (id,amount,currency,"name") VALUES (3,1000.00,'BRL','the mother fuc!@#!$ brush');
+INSERT INTO public.item (id,amount,currency,"name") VALUES (4,1000.00,'BRL','the mother fuc!@#!$ cleaner');
+INSERT INTO public.item (id,amount,currency,"name") VALUES (5,1000.00,'BRL','the mother fuc!@#!$ notes');
+INSERT INTO public.item (id,amount,currency,"name") VALUES (6,1000.00,'BRL','the mother fuc!@#!$ bag');
 
 
 
