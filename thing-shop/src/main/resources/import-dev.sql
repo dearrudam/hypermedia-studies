510f4b33001a08b87238afb40c579ee76ee51e83
Maximillian Arruda <dearrudam@gmail.com>
2019-09-17 02:40:27 -0300

    Added SQL scripts, but a test needs to be performed

---
+++ thing-shop/src/main/resources/import-dev.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.item (id,amount,currency,"name") VALUES (1,1000.00,'BRL','mother fucker pencil');
+INSERT INTO public.item (id,amount,currency,"name") VALUES (2,1000.00,'BRL','mother fucker pen');
+INSERT INTO public.item (id,amount,currency,"name") VALUES (3,1000.00,'BRL','mother fucker brush');
+INSERT INTO public.item (id,amount,currency,"name") VALUES (4,1000.00,'BRL','mother fucker cleaner');
+INSERT INTO public.item (id,amount,currency,"name") VALUES (5,1000.00,'BRL','mother fucker notes');
+INSERT INTO public.item (id,amount,currency,"name") VALUES (6,1000.00,'BRL','mother fucker bag');
+
+
+
+
+
